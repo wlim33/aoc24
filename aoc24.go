11bc84f3216035263a51a9daafa4572b4b3a7a0f
William Lim <limwilliam23@gmail.com>
2024-12-01 19:11:35 -0500

    day 1 complete

--- aoc24.go
+++ aoc24.go
@@ -9,25 +9,35 @@ import (
 
 type Solver interface {
 	Solve() error
+	LoadData(bool) error
 }
 
 func main() {
 	var day_flag int
-	flag.IntVar(&day_flag, "day", 1, "which day to run")
-	var input_size bool
-	flag.BoolVar(&input_size, "input_size", false, "which test data to use")
+	flag.IntVar(&day_flag, "d", 1, "Which day to run.")
+	var use_test_data bool
+	flag.BoolVar(&use_test_data, "t", false, "Use actual inputs instead of shared examples.")
 	flag.Parse()
 
 	var day_solver Solver
 	switch day_flag {
 	case 1:
-		fmt.Println("Selected day 1")
-		day_solver = &day1.Day1Solver{}
+		day_solver = new(day1.Day1Solver)
 	default:
 		fmt.Println("Selected day not implemented/released")
 	}
 
-	err := day_solver.Solve()
+	if use_test_data {
+		fmt.Printf("Selected day %d\n", day_flag)
+	} else {
+		fmt.Printf("Selected day %d(with example data)\n", day_flag)
+	}
+	err := day_solver.LoadData(use_test_data)
+	if err != nil {
+		log.Fatalf("Error trying to load data for day %d:\n%s", day_flag, err)
+	}
+
+	err = day_solver.Solve()
 	if err != nil {
 		log.Fatalf("Error trying to solve day %d:\n%s", day_flag, err)
 	}
--- day1/day1.go
+++ day1/day1.go
@@ -2,6 +2,7 @@ package day1
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"log"
 	"os"
@@ -9,10 +10,44 @@ import (
 	"strings"
 )
 
-type Day1Solver struct{}
+const SmallTestDataPath = "day1/small.txt"
+const LargeTestDataPath = "day1/full.txt"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+type Day1Solver struct {
+	file_name string
+}
+
+func (d *Day1Solver) LoadData(use_test_data bool) error {
+	if !use_test_data {
+		d.file_name = SmallTestDataPath
+	} else {
+		d.file_name = LargeTestDataPath
+	}
+	return nil
+}
 
 func (d *Day1Solver) Solve() error {
-	file, err := os.Open("day1/small.txt")
+	file, err := os.Open(d.file_name)
 	if err != nil {
 		return err
 	}
@@ -23,7 +58,11 @@ func (d *Day1Solver) Solve() error {
 	}()
 
 	scanner := bufio.NewScanner(file)
-	l_acc, r_acc := 0, 0
+	l_heap, r_heap := IntHeap{}, IntHeap{}
+	heap.Init(&l_heap)
+	heap.Init(&r_heap)
+
+	l_freq, r_freq := make(map[int]int), make(map[int]int)
 	for scanner.Scan() {
 		values := strings.Fields(scanner.Text())
 		var left int
@@ -37,9 +76,44 @@ func (d *Day1Solver) Solve() error {
 		if conv_err != nil {
 			return conv_err
 		}
-		l_acc += left
-		r_acc += right
+
+		heap.Push(&l_heap, left)
+		heap.Push(&r_heap, right)
+
+		if val, ok := l_freq[left]; ok {
+			l_freq[left] = val + 1
+		} else {
+			l_freq[left] = 1
+		}
+
+		if val, ok := r_freq[right]; ok {
+			r_freq[right] = val + 1
+		} else {
+			r_freq[right] = 1
+		}
+
 	}
-	fmt.Println("Solution: ", r_acc-l_acc)
+	total := 0
+	for l_heap.Len() > 0 && r_heap.Len() > 0 {
+		l := heap.Pop(&l_heap).(int)
+		r := heap.Pop(&r_heap).(int)
+		fmt.Println(l, r)
+		if l < r {
+			total += (r - l)
+		} else {
+			total += (l - r)
+		}
+	}
+	fmt.Println("Solution a: ", total)
+
+	similarity_score := 0
+	for k, v := range l_freq {
+
+		if val, ok := r_freq[k]; ok {
+			similarity_score += (v * val * k)
+		}
+	}
+
+	fmt.Println("Solution b: ", similarity_score)
 	return nil
 }
